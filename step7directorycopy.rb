f494ad5f08f486133b637b42ab6c7d4cd08599bf
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-05-19 15:52:07 +0100

    Committing Interactive Menu

--- step7directorycopy.rb
+++ step7directorycopy.rb
@@ -24,28 +24,7 @@ def input_students
   return students
 end
 
-def interactive_menu
-  students = []
-  loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-
-    selection = gets.chomp
-
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      printnames(students)
-      print_footer(students)
-    when "9"
-      exit
-    else
-      puts "I don't know what you meant, try again"
-    end
-  end
-end
-
-interactive_menu
+students = input_students
+print_header
+printnames(students)
+print_footer(students)
